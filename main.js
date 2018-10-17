dbc42f8c73ae729276b2cae84efca0b206406214
SeaCrap <506076395@qq.com>
2018-10-17 23:42:06 +0800

    自动播放

--- main.js
+++ main.js
@@ -3,18 +3,44 @@ console.log(allButtons)
 for(let i=0; i< allButtons.length; i++){
   $(allButtons[i]).on('click',(x)=>{
     var index = $(x.currentTarget).index()
-    var npx = index * -300
+    var npx = index * -400
     $('#images').css({
       transform: 'translate(' + npx + 'px)'
     })
   })
 }
 
+var n = 0;
+allButtons.eq(n%4).trigger('click')
+setInterval(()=>{
+  n += 1
+  allButtons.eq(n%4).trigger('click')
+}, 2000)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //每秒自动打印 0 ~ 3
-var n = 0;
-console.log(n%4)
-setInterval(()=>{
-  n+=1
-  console.log(n%4)
-},1000)
+//var n = 0;
+//console.log(n%4)
+//setInterval(()=>{
+//  n+=1
+//  console.log(n%4)
+//},1000)
