b9f18135fe6bc17701b03ef4493b21f73577c313
SeaCrap <506076395@qq.com>
2018-10-18 21:39:24 +0800

    封装完成

--- main.js
+++ main.js
@@ -10,45 +10,16 @@ for(let i=0; i< allButtons.length; i++){
     
     //点击按钮高亮
     var n = index
-    //这里也是激活button
-   // allButtons.eq(n)
-     // .addClass('red')
-     // .siblings('.red').removeClass('red')
     activeButton(allButtons.eq(n))
   })
 }
-
-
-
-
-
-
 
 
 //自动播放、按钮高亮
 var n = 0;
 var size = allButtons.length
 playSilde(n % size)
-var timerId = setInterval(()=>{// 闹钟去个名字
-  n += 1
-  playSilde(n % size) 
-}, 2000)
-
-function activeButton($button){//需要传入一个button，需要获取jquery对象，所以要$开头
-  // allButtons.eq(n%size).trigger('click')
- // $button.trigger('click')//trigger('click')可以放在外边(上边有了) 这里之激活按钮 
-    $button
-      .addClass('red')
-      .siblings('.red').removeClass('red')
-}
-
-function playSilde(index){
-  //按钮被点击之后添加class类 
-  //上边事件触发之后已经在处理了
-  // 这里就不需要再激活了
-  allButtons.eq(index).trigger('click')
-  //activeButton(allButtons.eq(index))//这里跟上边重复了
-}
+var timerId = setTimer()
 
 // 鼠标移入暂停、移开继续
 $('.window').on('mouseenter',()=>{
@@ -56,13 +27,30 @@ $('.window').on('mouseenter',()=>{
 })
 
 $('.window').on('mouseleave',()=>{
-  timerId = setInterval(()=>{// 闹钟去个名字
-    n += 1
-    playSilde(n % size) 
-  }, 2000)
+  timerId = setTimer()
 })
 
 
+
+//封装1：激活button 添加class类
+function activeButton($button){//需要传入一个button，需要获取jquery对象，所以要$开头
+    $button
+      .addClass('red')
+      .siblings('.red').removeClass('red')
+}
+
+//自动播放
+function playSilde(index){
+  allButtons.eq(index).trigger('click')
+}
+
+//设置闹钟
+function setTimer(){
+  return setInterval(()=>{// 闹钟去个名字
+    n += 1
+    playSilde(n % size) 
+  }, 2000)
+}
 
 
 
